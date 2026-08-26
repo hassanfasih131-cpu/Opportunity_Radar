da767adde2c0bd2bbf43742affef638c0bf45e70
Hassan Fasih <hassan.fasih131@gmail.com>
2026-08-26 09:31:18 -0700

    Added method to generate a dashboard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import requests
+from encodings import utf_8
 
+import requests
+import datetime
+from datetime import datetime
 #Public API containing job listings.
 url = "https://remotive.com/api/remote-jobs"
 def collect_data():
@@ -61,5 +64,98 @@ def filter_data(jobs):
     store.sort(key=lambda job: job["relevance_score"], reverse=True)
     print(f"{len(store)} jobs matched our criteria.")
     return store
-def generate_dashboard():
-    pass
+def generate_dashboard(options):
+    creation_time=str(datetime.now()) #The time the dashboard was created
+    #Storing the HTML code inside a variable to be used later in a .html file
+    html=f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Digital Marketing Opportunities Radar
+        </title>
+    </head>
+    <body>
+    <h1>
+            Digital Marketing Opportunities Radar
+    </h1>
+    <p>
+        <strong>Generated on {creation_time}</strong>
+    </p>
+    <p>
+        <strong>Data Source from Remotive</strong>
+    </p>
+    """
+    #if there are no opportunities
+    if not options:
+        html+="""
+        <p>
+            No opportunities was found
+        </p>
+        """
+    #Adding all the jobs found in the dashboard
+    for i in options:
+        title=i.get("title","unknown")
+        company_name=i.get("company_name","unknown")
+        category=i.get("category","unknown")
+        job_type=i.get("job_type","unknown")
+        location=i.get("candidate_required_location","unknown")
+        salary=i.get("salary","unknown")
+        URL=i.get("url","#")
+        score=i.get("relevance_score",0)
+        #Adding API into html
+        html += f"""
+            <div>
+
+                <h3>
+                    {title}
+                </h3>
+
+                <p>
+
+                    <strong>Company:</strong>
+                    {company_name}
+
+                    <br>
+
+                    <strong>Category:</strong>
+                    {category}
+
+                    <br>
+
+                    <strong>Job Type:</strong>
+                    {job_type}
+
+                    <br>
+
+                    <strong>Location:</strong>
+                    {location}
+
+                    <br>
+
+                    <strong>Salary:</strong>
+                    {salary}
+
+                    <br>
+
+                    <strong>Relevance Score:</strong>
+                    {score}
+
+                </p>
+
+                <a
+                    href="{url}"
+                    target="_blank"
+                >
+                    View Job on Remotive
+                </a>
+
+            </div>
+
+            <hr>
+        """
+        #Saving the HTML file
+        with open("GeneratedOutput.html","w",encoding="utf_8") as f:
+            f.write(html)
+        print("Dashboard generated")
